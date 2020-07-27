f11f6278a5b710d0e6b118c193d2c6ddb46e5c0e
rifqiiardhian <rifqiardhian@gmail.com>
2020-07-27 13:33:13 +0700

    upload1

---
+++ public/assets/js/mobiletest.js
@@ -0,0 +1,99 @@
+const dataurl = document.getElementById('url');
+const btn_check = document.getElementById('btn-check');
+const title = document.getElementById('result-title');
+const mob_issues_title = document.getElementById('mobile-issues-title');
+const mob_issues = document.getElementById('mobile-issues');
+const image = document.getElementById('result-image');
+const err_section = document.getElementById('error-section');
+const err_msg = document.getElementById('error-mdg');
+
+image.style = 'max-width:400px;height:auto';
+$(document).ready(function() {
+    $('#btn-check').on('click', function() {
+        var newData =
+        {
+            "url" : $('#url').val(),
+            "requestScreenshot": true
+        }
+        var dataJson = JSON.stringify(newData);
+
+        $.ajax({
+            url : "https://searchconsole.googleapis.com/v1/urlTestingTools/mobileFriendlyTest:run?key=AIzaSyAe7AXnQrH6VxQk6wDlg3E7eJuZn9AywC8",
+            type : "POST",
+            dataType : "JSON",
+            contentType: "application/json",
+            data : dataJson,
+            success : function(result) {
+                console.log(result);
+
+                if( result.testStatus.status === 'COMPLETE') {
+                    if(result.mobileFriendliness === 'MOBILE_FRIENDLY') {
+                        resultdata(result.mobileFriendliness, result.screenshot.data);
+                    } else {
+                        resultdata(result.mobileFriendliness, result.screenshot.data);
+                        mobileissues(result.mobileFriendlyIssues);
+                    }
+                } else {
+                    var errorstatus = result.testStatus.status;
+                    var errormessage = result.testStatus.detail;
+
+                    err_section.style = 'display:block';
+                    err_msg.innerHTML = errormessage;
+                }
+
+                resultdata(result.mobileFriendliness, result.screenshot.data);
+            },
+            error: function(e) {
+              console.log(e);
+
+              var errorstatus = result.testStatus.status;
+              var errormessage = result.testStatus.detail;
+
+              err_section.style = 'display:block';
+              err_msg.innerHTML = errormessage;
+            },
+        });
+    });
+});
+
+function resultdata(titledata, imagedata) {
+    if(titledata === 'MOBILE_FRIENDLY') {
+        title.innerHTML = 'Website Anda Mobile Friendly';
+    } else if(titledata === 'MOBILE_FRIENDLY_TEST_RESULT_UNSPECIFIED') {
+        title.innerHTML = 'Terjadi Kesalahan Saat Melakukan Test. Silakan Coba Lagi';
+    } else if(titledata === 'NOT_MOBILE_FRIENDLY') {
+        title.innerHTML = 'Website Anda Tidak Mobile Friendly';
+    }
+
+    var baseStr64 = imagedata;
+    image.src = "data:image/png;base64," + baseStr64;
+}
+
+function mobileissues(rules) {
+    var issues = '';
+    var issues_detail = '';
+    for (i = 0; i < rules.length; i++) {
+        // issues += rules[i].rule + "<br/>";
+
+        if( rules[i].rule === 'MOBILE_FRIENDLY_RULE_UNSPECIFIED') {
+            issues_detail += 'MOBILE FRIENDLY RULE UNSPECIFIED<br/>Unknown rule. Sorry, we dont have any description for the rule that was broken. <br/>';
+        } else if( rules[i].rule === 'USES_INCOMPATIBLE_PLUGINS') {
+            issues_detail += 'USES INCOMPATIBLE PLUGINS<br/>Plugins incompatible with mobile devices are being used <br/>';
+        } else if( rules[i].rule === 'CONFIGURE_VIEWPORT') {
+            issues_detail += 'CONFIGURE VIEWPORT<br/>Viewport is not specified using the meta viewport tag <br/>';
+        } else if( rules[i].rule === 'FIXED_WIDTH_VIEWPORT') {
+            issues_detail += 'FIXED WIDTH VIEWPORT<br/>Viewport defined to a fixed width <br/>';
+        } else if( rules[i].rule === 'SIZE_CONTENT_TO_VIEWPORT') {
+            issues_detail += 'SIZE CONTENT TO VIEWPORT<br/>Content not sized to viewport <br/>';
+        } else if( rules[i].rule === 'USE_LEGIBLE_FONT_SIZES') {
+            issues_detail += 'USE LEGIBLE FONT SIZES<br/>Font size is too small for easy reading on a small screen <br/>';
+        } else if( rules[i].rule === 'TAP_TARGETS_TOO_CLOSE') {
+            issues_detail += 'TAP TARGETS TOO CLOSE<br/>Touch elements are too close to each other <br/>';
+        }
+
+        // mob_issues_title.innerHTML = issues + "-" + issues_detail;
+        mob_issues.innerHTML = issues_detail;
+    }
+
+
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta charset="utf-8">
+        <title>Mobile Friendly Test</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+        integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/site.css') }}">
+        <link href="https://fonts.googleapis.com/css2?family=Poppins" rel="stylesheet">
+    </head>
+    <body>
+        <section class="home-section">
+            <div class="container-fluid container-home">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="content">
+                            <div class="form-group form-mobile">
+                                <div class="title text-center">Apakah website anda mobile friendly ?</div>
+                                <div class="subtitle text-center">Masukkan URL website anda dibawah ini</div>
+                                <input type="text" id="url" class="form-control input-url" placeholder="https://example.com"/>
+                                <div class="text-center">
+                                    <button class="btn btn-check" id="btn-check"><span class="check-arrow-right">CEK SEKARANG</span></button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="container-fluid container-info">
+                <div class="row success">
+                    <div class="col-md-6">
+                        <div class="title" id="result-title"></div>
+                        <h4 id="mobile-issues-title"></h4>
+                        <p id="mobile-issues"></p>
+                    </div>
+                    <div class="col-md-6 text-center">
+                        <img class="img-fluid img-result" id="result-image"/>
+                    </div>
+                </div>
+                <div class="row error" id="error-section">
+                    <div class="col-md-12">
+                        <h4 id="error-msg"></h4>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <script src="https://code.jquery.com/jquery-3.1.1.min.js">
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+        <script type="text/javascript" src="https://apis.google.com/js/client.js?onload=init"></script>
+
+        <script src="https://apis.google.com/js/api.js"></script>
+        <script src="{{ asset('assets/js/mobiletest.js') }}"></script>
+    </body>
+</html>
